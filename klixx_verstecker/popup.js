6289838af5f6841a291c7e0986917a41f65d6af5
Adrian Hett <adrian.hett@gmail.com>
2017-09-05 00:12:12 +0200

    removed unneccessary comments

--- klixx_verstecker/popup.js
+++ klixx_verstecker/popup.js
@@ -1,15 +1,3 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-
-/* function click(e) {
-  chrome.tabs.executeScript(null,
-      {code:".view-count.style-scope.yt-view-count-renderer='" + e.target.id + "'"});
-  window.close();
-} */
-
-
 
 function click(e){
   console.log("click " + e.target.id);
